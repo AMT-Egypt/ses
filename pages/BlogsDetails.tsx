586ba4fbd32d9b3a681be7c222451bf7b142d898
ahmedmansour779 <ahmedmansour1239@gmail.com>
2024-08-19 12:02:05 +0300

    add meta data

--- pages/BlogsDetails.tsx
+++ pages/BlogsDetails.tsx
@@ -1,7 +1,8 @@
+import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { blogsData } from '../public/data/blogs'
-import InnterPageHead from '../modules/organisms/inner_page_head'
 import { useEffect, useState } from 'react'
+import InnterPageHead from '../modules/organisms/inner_page_head'
+import { blogsData } from '../public/data/blogs'
 const BlogsDetails: React.FC = () => {
   const [category, setCategory] = useState<any>(null)
   const router = useRouter()
@@ -20,22 +21,29 @@ const BlogsDetails: React.FC = () => {
   }
 
   return (
-    <div>
-      <InnterPageHead title="Blogs Details" />
-      <div className="container mx-auto">
-        <h2 style={{ paddingBottom: '2rem' }} className="text-white mb-4">
-          {category.title}
-        </h2>
-        <div className="w-full flex-column gap-2">
-          {category.blogs.map((blog, index) => (
-            <div className="text-white" key={index}>
-              <h4 style={{ fontWeight: 'bold' }}>{blog.title}</h4>
-              <p>{blog.description}</p>
-            </div>
-          ))}
+    <>
+      <Head>
+        <meta
+          name="Keywords"
+          content={category.blogs[category.blogs.length - 1].description} />
+      </Head>
+      <div>
+        <InnterPageHead title="Blogs Details" />
+        <div className="container mx-auto">
+          <h2 style={{ paddingBottom: '2rem' }} className="text-white mb-4">
+            {category.title}
+          </h2>
+          <div className="w-full flex-column gap-2">
+            {category.blogs.map((blog, index) => (
+              <div className="text-white" key={index}>
+                <h4 style={{ fontWeight: 'bold' }}>{blog.title}</h4>
+                <p>{blog.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
